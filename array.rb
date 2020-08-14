cbf554cae9d6ed442cd764c38fa6331b73b6f24c
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-15 04:22:43 +0900

    配列から要素を取り出す方法

--- array.rb
+++ array.rb
@@ -1,7 +1,24 @@
-配列
-配列の定義
-users = ["Saitou", "Tanaka", "Suzuki", "Yamada"]
+# 配列
+# 配列の定義
+# users = ["Saitou", "Tanaka", "Suzuki", "Yamada"]
+# ages = [22, 34, 12, 45]
+
+配列から要素を取り出す
+ages = [22, 34, 12, 45]
+# 0はインデックス番号を示す
+puts ages[0]
+
 ages = [22, 34, 12, 45]
 
+# 0番目の要素：22を取り出す
+puts ages[0]
 
+# 1番目の要素：34を取り出す
+puts ages[1]
+
+# 2番目の要素：12を取り出す
+puts ages[2]
+
+# 3番目の要素：45を取り出す
+puts ages[3]
 
