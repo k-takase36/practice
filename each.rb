e8987e5d0f4ee8e0e15a5d495dfc067478bffe8f
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-17 04:07:31 +0900

    each文

---
+++ each.rb
@@ -0,0 +1,30 @@
+# each
+# eachは配列や範囲オブジェクトに対して使用することができる繰り返し処理です
+# 配列とは何個も要素を格納することができるもので、範囲オブジェクトとは0~100のように、数値的な範囲を表すデータのこと
+
+# users = ["satou", "tanaka", "yamada", "shimizu"]
+
+# # 配列に対してeach文で中身を表示
+# # usersの要素１つ１つをnameに代入してブロック内の処理を実行
+# users.each do |user|
+#   puts "Hello!! #{user}."
+# end
+
+# ages = [20, 60, 27, 23]
+
+# ages.each do |age|
+#   puts "I'm #{age} years old."
+# end
+
+# items = ["book", "food", "movie", "music"]
+
+# items.each do |item|
+#   puts "I bought #{item}"
+# end
+
+# prefectures = ["Hokkaido", "Okinawa", "Saitama", "Ibaraki"]
+
+# prefectures.each do |prefecture|
+#   puts "#{prefecture} is awesome!!"
+# end
+
