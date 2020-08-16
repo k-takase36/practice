428da566c067184414f5bddd75d19ac477a8eaf3
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-17 04:44:21 +0900

    each文プログラム

--- each.rb
+++ each.rb
@@ -56,3 +56,74 @@
 #   puts "No.#{i} #{prefecture} is awesome!!"
 # end
 
+# サンプルプログラム
+
+# チーム分けするメンバーの名前を格納する配列を定義
+# user_list = ["Yanagi", "Saitou", "Aoyagi", "Imai", "Obata", "Takahashi"]
+
+# # チームA,Bのメンバーを格納するための配列
+# team_a = []
+# team_b = []
+
+# # 配列を取り出すためのeach
+# user_list.each.with_index do |user, i|
+#   if i.odd?
+#     # 配列に要素を格納する
+#     team_a << user
+#   else
+#     # 配列に要素を格納する
+#     team_b << user
+#   end
+# end
+
+# # チームAのメンバーを表示
+# # \nを入れて改行
+# text = "チームA\n"
+# team_a.each.with_index(1) do |member, i|
+#   # テキストに文字列連結
+#   # チームメンバーを「〜さん」と表示する
+#   text += "NO.#{i}:#{member}さん\n"
+# end
+
+# # チームBのメンバーを表示
+# # \nを入れて改行
+# text += "\nチームB\n"
+# team_b.each.with_index(1) do |member, i|
+#   # テキストに文字列連結
+#   # チームメンバーを「〜さん」と表示する
+#   text += "No.#{i}:#{member}さん\n"
+# end
+
+# # チーム分けの情報を表示
+# puts text
+
+# shuffleメソッドを使用し、配列の各要素をランダムに並び替える
+user_list = ["Yanagi", "Saitou", "Aoyagi", "Imai", "Obata", "Takahashi"]
+
+# user_listの要素をランダムに入れ替える(破壊的メソッド)
+user_list.shuffle!
+
+team_a = []
+team_b = []
+
+user_list.each.with_index do |user, i|
+  if i.odd?
+    team_a << user
+  else
+    team_b << user
+  end
+end
+
+text = "チームA\n"
+# each.with_indexに変更
+team_a.each.with_index(1) do |member, i|
+  # 各メンバーに番号を振る
+  text += "No.#{i}：#{member}さん\n"
+end
+text += "\nチームB\n"
+team_b.each.with_index(1) do |member, i|
+  # 各メンバーに番号を振る
+  text += "No.#{i}：#{member}さん\n"
+end
+
+puts text
