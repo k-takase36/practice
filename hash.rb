9acb696d712ee213cbae2e75d035ea5534d98dbc
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-18 04:36:42 +0900

    ハッシュを使った繰り返し処理

--- hash.rb
+++ hash.rb
@@ -25,16 +25,41 @@
 # puts params[:address]
 
 # ハッシュの要素を編集
+# params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+
+# # データの書き換え
+# params[:name] = "Saitou"
+
+# # データの追加
+# params[:age] = 33
+
+# puts params[:name]
+# puts params[:age]
+
+# # 存在しないキーを指定するとnilになる
+# p params[:hobby]
+
+# # ハッシュを使った繰り返し処理
+# params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+
+# params.each do |key, value|
+#   puts "キー：#{key}"
+#   puts "バリュー：#{value}"
+# end
+
+# # each メソッドのブロック引数の数を１つにすると、ハッシュの中にキーとバリューが格納される
+# params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+
+# params.each do |key_value|
+#   # 配列を出力
+#   p key_value
+# end
+
 params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
 
-# データの書き換え
-params[:name] = "Saitou"
+params.each do |key_value|
+  # 配列を出力
+  puts key_value[0]
+  puts key_value[1]
+end
 
-# データの追加
-params[:age] = 33
-
-puts params[:name]
-puts params[:age]
-
-# 存在しないキーを指定するとnilになる
-p params[:hobby]
