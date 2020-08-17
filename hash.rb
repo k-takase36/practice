e305b6ea8b7ba5b78c9326125e17346271f482e8
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-18 04:45:35 +0900

    ハッシュのサイズ

--- hash.rb
+++ hash.rb
@@ -63,15 +63,19 @@
 #   puts key_value[1]
 # end
 
-# ハッシュの比較
-params1 = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
-params2 = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+# # ハッシュの比較
+# params1 = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+# params2 = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
 
-# trueが返る
-puts params1 == params2
+# # trueが返る
+# puts params1 == params2
 
-params3 = { name: "Tanabe", email: "hoge@co.ne.jp", address: "Tokyo" }
-params4 = { name: "Tanaka", email: "fuga@co.ne.jp", address: "Saitama" }
+# params3 = { name: "Tanabe", email: "hoge@co.ne.jp", address: "Tokyo" }
+# params4 = { name: "Tanaka", email: "fuga@co.ne.jp", address: "Saitama" }
 
-# falseが返る
-puts params3 == params4
+# # falseが返る
+# puts params3 == params4
+
+# ハッシュのサイズを調べる
+puts {}.size
+puts { name: "Tanabe", email: "hoge@co.ne.jp", address: "Tokyo" }.size
