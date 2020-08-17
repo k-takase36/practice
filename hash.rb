1a1d6ba021a480d0f9991dcb5a3fdd197f2c1cf2
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-18 04:19:44 +0900

    ハッシュの要素を編集

--- hash.rb
+++ hash.rb
@@ -16,10 +16,25 @@
 
 # 現場でもよく使用される書き方
 # 途中で改行できる
-params = {
-  name: "Tanaka",
-  email: "hoge@co.ne.jp",
-  address: "Saitama"
-}
+# params = {
+#   name: "Tanaka",
+#   email: "hoge@co.ne.jp",
+#   address: "Saitama"
+# }
 
-puts params[:address]
+# puts params[:address]
+
+# ハッシュの要素を編集
+params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+
+# データの書き換え
+params[:name] = "Saitou"
+
+# データの追加
+params[:age] = 33
+
+puts params[:name]
+puts params[:age]
+
+# 存在しないキーを指定するとnilになる
+p params[:hobby]
