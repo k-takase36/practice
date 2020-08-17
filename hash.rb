b212c35e564767d314c38914956448516635b0e4
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-18 05:03:57 +0900

    キーとバリューの入れ替え

--- hash.rb
+++ hash.rb
@@ -114,16 +114,21 @@
 # # => を使用した記述
 # { :name => :Tanaka, :email => :hoge@co.ne.jp, :address => :Saitama }
 
-# ハッシュのキーのみを取り出す
-params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+# # ハッシュのキーのみを取り出す
+# params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
 
-params.each_key do |key|
-  puts "キー：#{key}"
-end
+# params.each_key do |key|
+#   puts "キー：#{key}"
+# end
 
-# ハッシュのバリューのみを取り出す
-params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
+# # ハッシュのバリューのみを取り出す
+# params = { name: "Tanaka", email: "hoge@co.ne.jp", address: "Saitama" }
 
-params.each_value do |value|
-  puts "バリュー：#{value}"
-end
+# params.each_value do |value|
+#   puts "バリュー：#{value}"
+# end
+
+# キーとバリューの入れ替え
+params = { "name" => "Tanaka", "email" => "hoge@co.ne.jp", "address" => "Saitama" }
+
+puts params.invert
