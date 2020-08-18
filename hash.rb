658dc2ea36ef54531ceff8722dc214a54141a0be
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-08-19 05:10:57 +0900

    ハッシュプログラム作成

--- hash.rb
+++ hash.rb
@@ -128,7 +128,40 @@
 #   puts "バリュー：#{value}"
 # end
 
-# キーとバリューの入れ替え
-params = { "name" => "Tanaka", "email" => "hoge@co.ne.jp", "address" => "Saitama" }
+# # キーとバリューの入れ替え
+# params = { "name" => "Tanaka", "email" => "hoge@co.ne.jp", "address" => "Saitama" }
 
-puts params.invert
+# puts params.invert
+
+# ハッシュプログラム作成
+
+users = [ 
+          {name: :田中, age: 30, height: 160},
+          {name: :佐藤, age: 70, height: 150},
+          {name: :鈴木, age: 10, height: 120}
+        ]
+
+users.each do |user|
+
+  puts "----------------------"
+
+  puts "#{user[:name]}さん"
+  
+  if user[:age] <= 64
+    puts "年齢#{user[:age]}歳: 年齢確認OK"
+  else
+    puts "年齢#{user[:age]}歳: 年齢確認NG"
+  end
+
+  if user[:height] >= 130
+    puts "身長#{user[:height]}cm: 身長確認OK"
+  else
+    puts "身長#{user[:height]}cm: 身長確認NG"
+  end
+
+  if user[:age] <= 64 && user[:height] >= 130
+    puts "\nご利用いただけます"
+  else
+    puts "\nご利用いただけません"
+  end
+end
