7dd7d4486a798ee4c7f2681d7b4193815db03803
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-04-01 06:25:44 +0900

    array_java_create

--- array.java
+++ array.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    
+    String[] languages = {"Ruby","PHP", "Python"};
+
+    System.out.printlin(languages[1]);
+
+    languages[1] = "java";
+
+    System.out.printlin(languages[1]);
   }
 }
