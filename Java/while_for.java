8a430682102bc919c8e1c8bbbcf720e53c4dace1
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-04-02 07:05:56 +0900

    for_edit

--- Java/while_for.java
+++ Java/while_for.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     String[] names = {"にんじゃわんこ","ひつじ仙人", "ベイビーわんこ"};
 
-    for(int 1 = 0; names.length; i ++) {
+    for(int i = 0; i < names.length; i ++) {
       System.out.println("私の名前は" + names[i] + "です");
     }
   }
