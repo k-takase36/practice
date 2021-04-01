eb79bb7e3666d093004eee829cfec5784d42fae9
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-04-02 06:52:41 +0900

    for_create

---
+++ Java/while_for.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String[] names = {"にんじゃわんこ","ひつじ仙人", "ベイビーわんこ"};
+
+    for(int 1 = 0; names.length; i ++) {
+      System.out.println("私の名前は" + names[i] + "です");
+    }
+  }
+}
