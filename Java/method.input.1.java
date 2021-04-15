ee88f32e71a87fcdc7d846620486de7cde5d75b7
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-04-16 05:16:06 +0900

    method_input_scanner

---
+++ Java/method.input.1.java
@@ -0,0 +1,16 @@
+// java.util.Scannerを読み込む
+import java.util.Scanner;
+
+class Main {
+  public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("名前： ");
+    
+    // 変数nameを定義し、コンソールから文字列を受け取って代入
+    String name = scanner.next();
+    
+    // 「こんにちは◯◯さん」と出力
+    System.out.print("こんにちは" + name + "さん");
+  }
+}
