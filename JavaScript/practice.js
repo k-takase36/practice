136fff3f06a0b421fa83b3190f62a30d2df84410
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-11-29 09:27:13 +0900

    js-arrange-add

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -488,53 +488,52 @@
 
 // };
 
-// 自習2_ 親プルダウンの子要素を選択後、選択した子要素のプルダウンが表示
-// 条件：子要素のプルダウンは初期状態は非表示、親プルダウンで選択された時に表示される仕様
+// // 自習2_ 親プルダウンの子要素を選択後、選択した子要素のプルダウンが表示
+// // 条件：子要素のプルダウンは初期状態は非表示、親プルダウンで選択された時に表示される仕様
 
-// 手順
-// ①親プルダウンの親要素を取得
-// ②親プルダウンのvalue属性を代入
-// ③if文を使用して、親プルダウンの指定したvalue属性を選択したき、表示、非表示
+// // 手順
+// // ①親プルダウンの親要素を取得
+// // ②親プルダウンのvalue属性を代入
+// // ③if文を使用して、親プルダウンの指定したvalue属性を選択したき、表示、非表示
 
 
-let meatElm = document.getElementById('meat');
-// let meatChildElms = meatElm.children;
-meatElm.style.display = 'none';
+// let meatElm = document.getElementById('meat');
+// meatElm.style.display = 'none';
 
-let fishElm = document.getElementById('fish');
-// let fishChildElms = fishElm.children;
-fishElm.style.display = 'none';
+// let fishElm = document.getElementById('fish');
+// fishElm.style.display = 'none';
 
-let vegeElm = document.getElementById('vegetable');
-// let vegeChildElms = vegeElm.children;
-vegeElm.style.display = 'none';
+// let vegeElm = document.getElementById('vegetable');
+// vegeElm.style.display = 'none';
 
-function materialChange() {
-  let mateElm = document.getElementById('material');
-  let mateVal = mateElm.value; 
+// function materialChange() {
+//   let mateElm = document.getElementById('material');
+//   let mateVal = mateElm.value; 
   
-  if (mateVal == '2') {
-    meatElm.style.display = 'block';
-    fishElm.style.display = 'none';
-    vegeElm.style.display = 'none';
-  } else if (mateVal == '3') {
-    meatElm.style.display = 'none';
-    fishElm.style.display = 'block';
-    vegeElm.style.display = 'none';
-  } else if (mateVal == '4') {
-    meatElm.style.display = 'none';
-    fishElm.style.display = 'none';
-    vegeElm.style.display = 'block';
-  } else {
-    meatElm.style.display = 'none';
-    fishElm.style.display = 'none';
-    vegeElm.style.display = 'none';
-  }
+//   if (mateVal == '2') {
+//     meatElm.style.display = 'block';
+//     fishElm.style.display = 'none';
+//     vegeElm.style.display = 'none';
+//   } else if (mateVal == '3') {
+//     meatElm.style.display = 'none';
+//     fishElm.style.display = 'block';
+//     vegeElm.style.display = 'none';
+//   } else if (mateVal == '4') {
+//     meatElm.style.display = 'none';
+//     fishElm.style.display = 'none';
+//     vegeElm.style.display = 'block';
+//   } else {
+//     meatElm.style.display = 'none';
+//     fishElm.style.display = 'none';
+//     vegeElm.style.display = 'none';
+//   }
 
-};
+// };
 
-// addEventListenerの方法
-  // let matElm = document.getElementById('material');
-  // matElm.addEventListener('change', () => {
-  //   let matChildElms = matElm.children;
-  // });
+// 学習3_通知設定（ラジオボタン）側のHTML作成
+// 学習4_
+
+手順
+①親プルダウンの親要素を取得
+②親プルダウンのvalue属性を代入
+③if文を使用して、親プルダウンの指定したvalue属性を選択したき、表示、非表示
