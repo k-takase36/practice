43344c6c1f5efea844f800fb1803a34390e606dd
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-12-02 10:57:24 +0900

    js-self-study2-add

---
+++ JavaScript/js_practice2/practice_2.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// 自習2（課題3アレンジ）_ボタンクリックすると、プルダウンAの子要素を、プルダウンBの子要素の上部へ追加
+
+// ◆手順
+// ①A親要素を取得
+// ②A子要素を取得
+// ③B親要素を取得
+// ④B子要素を取得
+// ⑤B親要素の先頭に、②子要素を昇順で追加(for文)
+// 【for文の中】
+// ⑥for文の条件を記載（変数定義、繰り返し回数を指定、カウンタ指定）
+// ⑦A子要素のindex「0」は追加しない処理を記載（if break使用→ index「0」になったらfor文を処理を抜ける処理）
+// ⑧B子要素に追加する要素を生成
+// ⑨追加する要素⑧に、A子要素のtext、valueを設定（追加）
+// ⑩B親要素のindex「0」の下に指定して、⑧子要素を追加
+
+function cookChange() {
+  let cookAElm = document.getElementById('cookA');
+  let cookChildAElms = cookAElm.children;
+  let cookBElm = document.getElementById('cookB');
+  let cookChildBElms = cookBElm.children;
+
+  for (let i=3; i<cookChildAElms.length; i--) {
+    if (i == 0) {
+      break;
+    }
+    let addElm = document.createElement('option');
+    addElm.text = cookChildAElms[i].text;
+    addElm.value = cookChildAElms[i].value;
+    cookBElm.insertBefore(addElm, cookChildBElms[1]);
+  }
+
+};
